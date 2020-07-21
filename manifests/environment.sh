3d8baef8e88a29ada39d8038d0febf25e71c3e09
sei0jieai <35719255+sei0jieai@users.noreply.github.com>
2020-07-21 10:34:58 +0800

    1
    123

--- manifests/environment.sh
+++ manifests/environment.sh
@@ -4,19 +4,19 @@
 export ENCRYPTION_KEY=$(head -c 32 /dev/urandom | base64)
 
 # 集群各机器 IP 数组
-export NODE_IPS=(172.27.138.251 172.27.137.229 172.27.138.239)
+export NODE_IPS=(172.16.1.202 10.10.1.101 10.10.1.103)
 
 # 集群各 IP 对应的主机名数组
-export NODE_NAMES=(zhangjun-k8s-01 zhangjun-k8s-02 zhangjun-k8s-03)
+export NODE_NAMES=(stage-lab-api stage-mod lab-mod)
 
 # etcd 集群服务地址列表
-export ETCD_ENDPOINTS="https://172.27.138.251:2379,https://172.27.137.229:2379,https://172.27.138.239:2379"
+export ETCD_ENDPOINTS="https://172.16.1.202:2379,https://10.10.1.101:2379,https://10.10.1.103:2379"
 
 # etcd 集群间通信的 IP 和端口
-export ETCD_NODES="zhangjun-k8s-01=https://172.27.138.251:2380,zhangjun-k8s-02=https://172.27.137.229:2380,zhangjun-k8s-03=https://172.27.138.239:2380"
+export ETCD_NODES="stage-lab-api=https://172.16.1.202:2380,stage-mod=https://10.10.1.101:2380,lab-mod=https://10.10.1.103:2380"
 
 # kube-apiserver 的反向代理(kube-nginx)地址端口
-export KUBE_APISERVER="https://127.0.0.1:8443"
+export KUBE_APISERVER="https://127.0.0.1:38443"
 
 # 节点间互联网络接口名称
 export IFACE="eth0"
@@ -40,7 +40,7 @@ export CONTAINERD_DIR="/data/k8s/containerd"
 ## 以下参数一般不需要修改
 
 # TLS Bootstrapping 使用的 Token，可以使用命令 head -c 16 /dev/urandom | od -An -t x | tr -d ' ' 生成
-BOOTSTRAP_TOKEN="41f7e4ba8b7be874fcff18bf5cf41a7c"
+BOOTSTRAP_TOKEN="c99c6485919552ee0a30ec15af9f8baf"
 
 # 最好使用 当前未用的网段 来定义服务网段和 Pod 网段
 
@@ -48,7 +48,7 @@ BOOTSTRAP_TOKEN="41f7e4ba8b7be874fcff18bf5cf41a7c"
 SERVICE_CIDR="10.254.0.0/16"
 
 # Pod 网段，建议 /16 段地址，部署前路由不可达，部署后集群内路由可达(flanneld 保证)
-CLUSTER_CIDR="172.30.0.0/16"
+CLUSTER_CIDR="192.30.0.0/16"
 
 # 服务端口范围 (NodePort Range)
 export NODE_PORT_RANGE="30000-32767"
@@ -60,7 +60,7 @@ export CLUSTER_KUBERNETES_SVC_IP="10.254.0.1"
 export CLUSTER_DNS_SVC_IP="10.254.0.2"
 
 # 集群 DNS 域名（末尾不带点号）
-export CLUSTER_DNS_DOMAIN="cluster.local"
+export CLUSTER_DNS_DOMAIN="xp.local"
 
 # 将二进制目录 /opt/k8s/bin 加到 PATH 中
 export PATH=/opt/k8s/bin:$PATH
